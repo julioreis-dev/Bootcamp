95d3392f0c87c994071e7fb9c2b087073b2a16c5
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-12-04 23:34:26 -0300

    Add files via upload

---
+++ emailbirthday.py
@@ -0,0 +1,29 @@
+import smtplib
+import datetime as dt
+import random
+import pandas as pd
+
+
+my_email = 'jrfirmino01@gmail.com'
+my_password = 'Manuela01*'
+
+now = dt.datetime.now()
+tuplebirth = (now.month, now.day)
+
+data = pd.read_csv('Solution+-+birthday-wisher-end/birthdays.csv')
+birthdays_dict = {(dados["month"], dados["day"]): dados for (index, dados) in data.iterrows()}
+
+if tuplebirth in birthdays_dict:
+    birth_name = birthdays_dict[tuplebirth]
+    file_path = f'Solution+-+birthday-wisher-end/letter_templates/letter_{random.randint(1,3)}.txt'
+
+    with open(file_path, encoding='utf8') as content:
+        contents = content.read()
+        contents = contents.replace("[NAME]", birth_name['name'])
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as conection:
+        conection.starttls()
+        conection.login(my_email, my_password)
+        conection.sendmail(from_addr=my_email,
+                           to_addrs='juliofirminoreis@hotmail.com',
+                           msg=f'Subject:Feliz aniversario!!!\n\n{contents}')
